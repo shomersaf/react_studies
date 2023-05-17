693d1145506ddf2acaed22c0d0a09abf467cf976
shomersaf <shomersaf@gmail.com>
2023-05-17 12:43:10 +0300

    button

--- vite_project/vite-project/src/components/Unit/index.tsx
+++ vite_project/vite-project/src/components/Unit/index.tsx
@@ -1,41 +1,42 @@
 import { useState, useEffect } from "react";
 
-function Unit(props:any){
-    
-    const [rankClass, setRankClass]= useState("");
-    const [expenseRankText, setRankTest]= useState("");
-  
-    useEffect(()=>{
-        console.log("useEffect")
+function Unit(props: any) {
+
+    const [rankClass, setRankClass] = useState("");
+    const [expenseRankText, setRankTest] = useState("");
+
+    useEffect(() => {
+
         // props.iAmount>500?setRankClass("highExpense"):setRankClass("lowExpense")
-        if(props.iAmount>500){
+        if (props.iAmount > 500) {
             setRankClass("highExpense")
             setRankTest("High Expense")
-        }else{
+        } else {
             setRankClass("lowExpense")
             setRankTest("Low Expense")
         }
     })
-    return(
-        <div key={props.iName} className="expensesListItem">
-        <h3>
-        {props.iName}
-        </h3>
-        <div className="dataDiv">
-        <div className="leftDiv">
-        <div>{props.iDate}</div>
-        <div className ={rankClass}>{expenseRankText}</div>
-        <div className = "category">{props.iCategory}</div>
-        </div>
-        <div className="rightDiv">
-        <div><strong>${props.iAmount}</strong></div>
-        <div className="junk">
-            <img src="\src\assets\junk-box-svgrepo-com.svg" alt="junk icon" />
-        </div>
-        </div>
-        </div>  
-    </div>
-       
+    return (
+        <>
+            <h3>
+                {props.iName}
+            </h3>
+            <div className="dataDiv">
+                <div className="leftDiv">
+                    <div>{props.iDate}</div>
+                    <div className={rankClass}>{expenseRankText}</div>
+                    <div className="category">{props.iCategory}</div>
+                </div>
+                <div className="rightDiv">
+                    <div><strong>${props.iAmount}</strong></div>
+
+                    <button className="junkButton"> <img src="\src\assets\junk-box-svgrepo-com.svg" alt="junk icon" /></button>
+
+
+                </div>
+            </div>
+        </>
+
     )
 
 }
